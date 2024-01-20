3b7495bb32d0292c72bb0fe222631db7d2e88e08
Daniel Mattivi <123580671+DanielMattivi@users.noreply.github.com>
2024-01-20 08:46:17 -0300

    Update TestaExcluirVenda.java

--- DBEPROG(20.0)/test/TestaTudo/TestaExcluirVenda.java
+++ DBEPROG(20.0)/test/TestaTudo/TestaExcluirVenda.java
@@ -1,16 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package TestaTudo;
 
 import DAO.VendaDAO;
 
-/**
- *
- * @author 08050438
- */
+
 public class TestaExcluirVenda {
     public static void main(String[] args) {
         
