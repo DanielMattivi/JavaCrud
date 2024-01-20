590187b3539aea58a8f1c3e0babaf6ac367ce769
Daniel Mattivi <123580671+DanielMattivi@users.noreply.github.com>
2024-01-20 08:46:55 -0300

    Update TestaInserirProduto.java

--- DBEPROG(20.0)/test/TestaTudo/TestaInserirProduto.java
+++ DBEPROG(20.0)/test/TestaTudo/TestaInserirProduto.java
@@ -1,18 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package TestaTudo;
 
 import DAO.FornecedorDAO;
 import DAO.ProdutoDAO;
 import model.Fornecedor;
 import model.Produto;
-/**
- *
- * @author 08050438
- */
+
+
 public class TestaInserirProduto {
     public static void main(String[] args) {
         Fornecedor fr = (Fornecedor) new FornecedorDAO().pesquisar(1);
