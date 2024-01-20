df79f0ab42911dba47b211cb0bd6e9fbfdff5799
Daniel Mattivi <123580671+DanielMattivi@users.noreply.github.com>
2024-01-20 08:49:28 -0300

    Update VendaDAO.java

--- DBEPROG(20.0)/src/DAO/VendaDAO.java
+++ DBEPROG(20.0)/src/DAO/VendaDAO.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package DAO;
 
 
@@ -13,10 +9,7 @@ import model.Cliente;
 import model.Funcionario;
 import model.Produto;
 
-/**
- *
- * @author 08050440 */
- 
+
 public class VendaDAO implements OperacoesDAO{
     
     private static final Connection con = Conexao.getConexao();
