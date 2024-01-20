84ed9644ea47f1a775189dd4fb43383dd064bc8e
Daniel Mattivi <123580671+DanielMattivi@users.noreply.github.com>
2024-01-20 08:50:16 -0300

    Update Conexao.java

--- DBEPROG(20.0)/src/persist/Conexao.java
+++ DBEPROG(20.0)/src/persist/Conexao.java
@@ -1,14 +1,6 @@
 package persist;
 import java.sql.*;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-/**
- *
- * 08050440
- */ 
 public class Conexao {
 
     public static Connection getConexao(){
