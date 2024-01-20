8ff752094d1c1e39ec6e62f34db5ee80c00d548e
Daniel Mattivi <123580671+DanielMattivi@users.noreply.github.com>
2024-01-20 08:45:05 -0300

    Update TestaConexao.java

--- DBEPROG(20.0)/test/TestaTudo/TestaConexao.java
+++ DBEPROG(20.0)/test/TestaTudo/TestaConexao.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package TestaTudo;
 
 import java.sql.*;
 import persist.*;
 
-/**
- *
- * @author 08050440
- */
+
 public class TestaConexao {
     
     public static void main(String[] args) {
