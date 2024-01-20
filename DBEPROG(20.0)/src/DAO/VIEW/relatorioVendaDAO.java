4014d7a52a47bb967a81b10baeff093e67bb9fbc
Daniel Mattivi <123580671+DanielMattivi@users.noreply.github.com>
2024-01-20 08:48:53 -0300

    Update relatorioVendaDAO.java

--- DBEPROG(20.0)/src/DAO/VIEW/relatorioVendaDAO.java
+++ DBEPROG(20.0)/src/DAO/VIEW/relatorioVendaDAO.java
@@ -1,17 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package DAO.VIEW;
 
 import java.sql.*;
 import java.util.ArrayList;
 import persist.Conexao;
 
-/**
- *
- * @author LeKKi
- */
+
 public class relatorioVendaDAO {
 
     public static ArrayList emitirRelatorio() {
