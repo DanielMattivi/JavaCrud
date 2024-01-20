db9a87939567bbb6a7f448350dcb287a159cd658
Daniel Mattivi <123580671+DanielMattivi@users.noreply.github.com>
2024-01-20 08:49:18 -0300

    Update OperacoesDAO.java

--- DBEPROG(20.0)/src/DAO/OperacoesDAO.java
+++ DBEPROG(20.0)/src/DAO/OperacoesDAO.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package DAO;
 
-/**
- *
- * @author mk
- */
+
 public interface OperacoesDAO {
     
     public boolean inserir (Object obj);
