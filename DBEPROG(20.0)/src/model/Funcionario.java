a2a19b6485a8b2b03f947f1831e25f1e9c6cb67a
Daniel Mattivi <123580671+DanielMattivi@users.noreply.github.com>
2024-01-20 08:50:04 -0300

    Update Funcionario.java

--- DBEPROG(20.0)/src/model/Funcionario.java
+++ DBEPROG(20.0)/src/model/Funcionario.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package model;
-/**
- *
- * @author 08050461
- */
+
 public class Funcionario {
     
     private String nome;
