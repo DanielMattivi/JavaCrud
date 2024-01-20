e49fa5228a4979e81e704f337a7deeb95ba55728
Daniel Mattivi <123580671+DanielMattivi@users.noreply.github.com>
2024-01-20 08:45:36 -0300

    Update TestaEditarFuncionario.java

--- DBEPROG(20.0)/test/TestaTudo/TestaEditarFuncionario.java
+++ DBEPROG(20.0)/test/TestaTudo/TestaEditarFuncionario.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package TestaTudo;
 
 import DAO.FuncionarioDAO;
 import model.Funcionario;
 
-/**
- *
- * @author LeKKi
- */
+
 public class TestaEditarFuncionario {
     
     public static void main(String[] args) {
