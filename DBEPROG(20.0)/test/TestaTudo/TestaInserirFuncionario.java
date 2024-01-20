aa8a4fdfa76d81c533306b2c73fede5da96d42f4
Daniel Mattivi <123580671+DanielMattivi@users.noreply.github.com>
2024-01-20 08:46:46 -0300

    Update TestaInserirFuncionario.java

--- DBEPROG(20.0)/test/TestaTudo/TestaInserirFuncionario.java
+++ DBEPROG(20.0)/test/TestaTudo/TestaInserirFuncionario.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package TestaTudo;
 
 import DAO.FuncionarioDAO;
 import model.Funcionario;
 
-/**
- *
- * @author Arthur
- */
 
 public class TestaInserirFuncionario {
     public static void main(String[] args) {
