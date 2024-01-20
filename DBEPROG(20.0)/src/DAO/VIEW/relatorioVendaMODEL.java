96e2a222e6e6c1c0c0df7c068124ace0ba55d887
Daniel Mattivi <123580671+DanielMattivi@users.noreply.github.com>
2024-01-20 08:49:01 -0300

    Update relatorioVendaMODEL.java

--- DBEPROG(20.0)/src/DAO/VIEW/relatorioVendaMODEL.java
+++ DBEPROG(20.0)/src/DAO/VIEW/relatorioVendaMODEL.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package DAO.VIEW;
 
-/**
- *
- * @author LeKKi
- */
+
 public class relatorioVendaMODEL {
    
     private String modelo;
